66edd5f2b795515108ede6cfbfd86c18275fea9c
Carlos <carloshenrgin@ufu.br>
2023-05-06 14:10:46 -0300

    feature: API routes for kins added. Kin Schema updated

--- server/models/kin.js
+++ server/models/kin.js
@@ -4,9 +4,37 @@ const { Schema } = mongoose;
 const { schema: talentSchema } = require("./talent");
 
 const kinSchema = new Schema({
-  name: String,
-  attribute: String,
-  talent: talentSchema,
+  name: {
+    type: String,
+    required: [true, "Kin must have a non-empty name"],
+    trim: true,
+    validate: {
+      validator: function (name) {
+        return name.trim().length > 0;
+      },
+      message: "Name must have a non-empty value",
+    },
+  },
+  attribute: {
+    type: String,
+    trim: true,
+    required: [true, "Key attribute required"],
+    validate: {
+      validator: function (attribute) {
+        return ["Strength", "Agility", "Wits", "Empathy"].includes(attribute);
+      },
+      message: "Invalid attribute",
+    },
+  },
+  talent: {
+    type: talentSchema,
+    required: true,
+    validate: {
+      validator: function (talent) {
+        return talent.type === "Kin";
+      },
+    },
+  },
   youngAge: Number,
   adultAge: Number,
   oldAge: Number,
--- server/routes/kin.js
+++ server/routes/kin.js
@@ -1,15 +1,38 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
 const { model: Kin } = require("../models/kin");
+const { model: Talent } = require("../models/talent");
+const sanitizeFormData = require("../utils/sanitizeFormData");
+
+router.use(bodyParser.urlencoded({ extended: true }));
 
 router.get("/register", (req, res) => {
-  res.sendFile(__dirname + "/registerKin.html");
+  res.sendFile("/registerKin.html", { root: __dirname });
 });
 
 router.post("/register", async (req, res) => {
-  const kin = new Kin({});
+  const kinData = sanitizeFormData(req.body);
 
-  await kin.save();
+  kinData.talent = await Talent.findOne({ kin: kinData.name });
+
+  let kin = new Kin(kinData);
+
+  try {
+    kin = await kin.save();
+
+    res.json(kin);
+  } catch (err) {
+    console.error(err);
+
+    let errorString = "";
+    for (let error in err.errors) {
+      errorString += `<li>${error}</li>`;
+    }
+    res.write("<h1>Errors</h1>");
+    res.write(`<ul>${errorString}</ul>`);
+    res.send();
+  }
 });
 
 module.exports = router;
